928006024df8cdc843c6391b08fd442ed41c1d61
theedrifter <minhguyenn@gmail.com>
2018-03-03 17:05:44 -0600

    fix node bug 1

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ router.use(function(req, res, next) {
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
+/*router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
-});
+});*/
 
 // on routes that end in /bears
 // ----------------------------------------------------
